2dd5fdefd276d47694c2938055a6cdcd5359f270
KyeongKyu <kyeongkyu.lee@gmail.com>
2026-03-22 07:26:10 +0900

    feature/프로젝트 셋팅

---
+++ imagesearch/imagesearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  imagesearchApp.swift
+//  imagesearch
+//
+//  Created by ekyu on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct imagesearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
